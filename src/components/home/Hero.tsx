d43d852a283b4e273a139107a996c31088af29e5
JaydipJadhav25 <jaydipdjadhav@gmail.com>
2025-08-01 14:40:24 +0530

    update

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -23,28 +23,7 @@ const Hero = () => {
   const isMobail = useIsMobile();
   console.log("ismobail  : ", isMobail)
 
-   const slideData = [
-    {
-      title: "Mystic Mountains",
-      button: "Explore Component",
-      src: "https://images.unsplash.com/photo-1494806812796-244fe51b774d?q=80&w=3534&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      title: "Urban Dreams",
-      button: "Explore Component",
-      src: "https://images.unsplash.com/photo-1518710843675-2540dd79065c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      title: "Neon Nights",
-      button: "Explore Component",
-      src: "https://images.unsplash.com/photo-1590041794748-2d8eb73a571c?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      title: "Desert Whispers",
-      button: "Explore Component",
-      src: "https://images.unsplash.com/photo-1679420437432-80cfbf88986c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-  ];
+
 
 
 
@@ -142,7 +121,7 @@ const Hero = () => {
         <div className="flex flex-wrap items-center gap-3 pt-6" >
           {["React", "Python", "AWS", "Machine Learning", "Blockchain"].map((tech, index) => (
             // <span key={tech} className="px-3 py-1 text-sm bg-muted rounded-full" data-aos="zoom-in" data-aos-delay={500 + index * 100}>
-                    <span key={tech} className="px-3 py-1 text-sm bg-muted rounded-full"> 
+                    <span key={index+tech} className="px-3 py-1 text-sm bg-muted rounded-full"> 
               {tech}
             </span>
           ))}
